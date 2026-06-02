7bf17689e92292f3b9a1fde3a8b0fb06354e1a86
26danilova26-oss <26danilova26@gmail.com>
2026-06-03 02:15:35 +0300

    Реализована логика переключения календаря вместе с неделей, но появилась тупая ошибка

--- lib/src/controller/week_controller.dart
+++ lib/src/controller/week_controller.dart
@@ -4,21 +4,52 @@ import '../model/week_calculator.dart';
 
 class WeekController extends ChangeNotifier {
   final WeekCalculator _calculator = WeekCalculator();
-  late Week _currentWeek;
 
-  WeekController() {
-    _currentWeek = _calculator.getCurrentWeek();
+  int _weekOffset = 0;
+  Week _baseWeek;
+
+  WeekController() : _baseWeek = WeekCalculator().getCurrentWeek();
+
+  int get weekOffset => _weekOffset;
+
+  Week get currentWeek => getWeekAtOffset(_weekOffset);
+
+  Week getWeekAtOffset(int offset) {
+    return Week(_baseWeek.startMonday.add(Duration(days: 7 * offset)));
   }
 
-  Week get currentWeek => _currentWeek;
+  void setWeekOffset(int offset) {
+    if (_weekOffset != offset) {
+      _weekOffset = offset;
+      notifyListeners();
+    }
+  }
 
   void previousWeek() {
-    _currentWeek = _calculator.getPreviousWeek(_currentWeek);
+    _weekOffset--;
     notifyListeners();
   }
 
   void nextWeek() {
-    _currentWeek = _calculator.getNextWeek(_currentWeek);
+    _weekOffset++;
     notifyListeners();
+  }
+
+  void resetToCurrent() {
+    _baseWeek = _calculator.getCurrentWeek();
+    _weekOffset = 0;
+    notifyListeners();
+  }
+
+  /// Переключает неделю так, чтобы она содержала указанную дату
+  void goToDate(DateTime date) {
+    // Определяем понедельник для выбранной даты
+    final monday = DateTime(date.year, date.month, date.day);
+    final offsetDays = monday.difference(_baseWeek.startMonday).inDays;
+    final newOffset = offsetDays ~/ 7; // целое число недель
+    if (newOffset != _weekOffset) {
+      _weekOffset = newOffset;
+      notifyListeners();
+    }
   }
 }
