8da57879a78f08bda4d6bb45dd89e9b95609861c
26danilova26-oss <26danilova26@gmail.com>
2026-06-03 02:02:06 +0300

    Установлена локаль, потому что при запуске календаря приложение падает

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const TodoApp());
-}
-
-class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To-Do List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const Scaffold(
-        body: Center(child: Text('TODO App')),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'src/model/sqlite_task_repository.dart';
 import 'src/controller/week_controller.dart';
 import 'src/controller/task_controller.dart';
 import 'src/presentation/pages/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -61,6 +39,13 @@ class TodoApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
       ),
+      locale: const Locale('ru'),
+      supportedLocales: const [Locale('ru')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       home: HomePage(
         taskController: taskController,
         weekController: weekController,
