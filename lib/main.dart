88128dacec788753101c113ea1808cb37dab14fe
26danilova26-oss <26danilova26@gmail.com>
2026-06-03 00:57:40 +0300

    Подключаем БД, контроллеры и домашнюю страницу

--- lib/main.dart
+++ lib/main.dart
@@ -21,4 +21,51 @@ class TodoApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     );
   }
+}import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'src/model/sqlite_task_repository.dart';
+import 'src/controller/week_controller.dart';
+import 'src/controller/task_controller.dart';
+import 'src/presentation/pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ru', null);
+
+  final database = await SqliteTaskRepository.createDatabase();
+  final taskRepository = SqliteTaskRepository(database);
+  final taskController = TaskController(taskRepository);
+  final weekController = WeekController();
+
+  runApp(TodoApp(
+    taskController: taskController,
+    weekController: weekController,
+  ));
+}
+
+class TodoApp extends StatelessWidget {
+  final TaskController taskController;
+  final WeekController weekController;
+
+  const TodoApp({
+    super.key,
+    required this.taskController,
+    required this.weekController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: HomePage(
+        taskController: taskController,
+        weekController: weekController,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
